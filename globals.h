37e561b1149085311c9ff2fac34bbafd8094eb32
c-moi <emmanuelbert7@gmail.com>
2023-05-03 11:16:21 +0200

    osef

--- globals.h
+++ globals.h
@@ -8,6 +8,8 @@ typedef struct move {
     char position[3]; 
     int joueur; // joueur qui a joué le coup (1 jeton noir ou 2 jeton blanc)
 
+    struct move *switched;
+    
     struct move *suiv;
     struct move *prec;
 } Move;
